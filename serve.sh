e122a6ba3ba5c10b59bb7c6ce63644d2b67f2a35
Oliver Lambson <oliverlambson@gmail.com>
2020-07-10 18:48:38 +0100

    Moved and renamed app notebook

--- serve.sh
+++ serve.sh
@@ -1,3 +1,3 @@
 # enable_nbextensions    --> allow Plotly
 # TagRemovePreprocessor  --> don't display cells tagged with 'hide'  
-voila Scratchpads/Admissions_generation.ipynb --enable_nbextensions=True --TagRemovePreprocessor.remove_cell_tags='{"hide"}'
+voila app.ipynb --enable_nbextensions=True --TagRemovePreprocessor.remove_cell_tags='{"hide"}'
