61290ec381d716ffdc3a215e6ad7189114054d7e
Oliver Lambson <oliverlambson@gmail.com>
2020-07-12 23:07:24 +0100

    Serve with debug

--- serve.sh
+++ serve.sh
@@ -1,3 +1,4 @@
 # enable_nbextensions    --> allow Plotly
 # TagRemovePreprocessor  --> don't display cells tagged with 'hide'  
-voila app.ipynb --enable_nbextensions=True --TagRemovePreprocessor.remove_cell_tags='{"hide"}'
+# debug                  --> show error outputs
+voila app.ipynb --enable_nbextensions=True --TagRemovePreprocessor.remove_cell_tags='{"hide"}' --debug
