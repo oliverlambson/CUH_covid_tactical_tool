e542dffcbc69fca437ea6b097cf3ffcc546001bb
Oliver Lambson <oliverlambson@gmail.com>
2020-07-16 16:07:46 +0100

    Include robots.txt in voila serve

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: viola --port:$PORT --no-browser --enable_nbextensions=True --TagRemovePreprocessor.remove_cell_tags='{"hide"}' app.ipynb
+web: voila --port:$PORT --no-browser --enable_nbextensions=True --TagRemovePreprocessor.remove_cell_tags='{"hide"}' --VoilaConfiguration.file_whitelist="['(app.ipynb)|(robots.txt)']" app.ipynb
--- serve.sh
+++ serve.sh
@@ -1,4 +1,11 @@
 # enable_nbextensions    --> allow Plotly
-# TagRemovePreprocessor  --> don't display cells tagged with 'hide'  
+# TagRemovePreprocessor  --> don't display cells tagged with 'hide' 
+# file_whitelist         --> serve app.ipynb and robots.txt
 # debug                  --> show error outputs
-voila app.ipynb --enable_nbextensions=True --TagRemovePreprocessor.remove_cell_tags='{"hide"}' --debug
+
+voila \
+    --enable_nbextensions=True \
+    --TagRemovePreprocessor.remove_cell_tags='{"hide"}' \
+    --VoilaConfiguration.file_whitelist="['(app.ipynb)|(robots.txt)']" \
+    --debug \
+    app.ipynb
